8f1226523e8813ad7d521c54341c99b6a86abda0
John Pope <john.pope@wweevv.app>
2022-09-08 11:16:51 +1000

    ok

--- submodules/ContactListUI/Sources/WEVRootNode.swift
+++ submodules/ContactListUI/Sources/WEVRootNode.swift
@@ -26,6 +26,8 @@ import Alamofire
 import GalleryUI
 import Postbox
 import TelegramCore
+import InstantPageUI
+
 
 public class WEVRootNode: ASDisplayNode{
     let contactListNode: ContactListNode
@@ -254,32 +256,28 @@ extension WEVRootNode: UICollectionViewDataSource {
         let video = showDataArray[indexPath.row]
         print("video:",video)
         
-
-
-       
-        
-        // ORIGINAL
-//        let message = Message(stableId: 0, stableVersion: 0, id: MessageId(peerId: PeerId(0), namespace: Namespaces.Message.Local, id: 0), globallyUniqueId: nil, groupingKey: nil, groupInfo: nil, threadId: nil, timestamp: 0, flags: [], tags: [], globalTags: [], localTags: [], forwardInfo: nil, author: nil, text: "", attributes: [], media: [file.file.media], peers: SimpleDictionary(), associatedMessages: SimpleDictionary(), associatedMessageIds: [], associatedMedia: [:])
 //
-        //JP HACK
-        let message = Message(stableId: 0, stableVersion: 0, id: MessageId(peerId: PeerId(0), namespace: Namespaces.Message.Local, id: 0), globallyUniqueId: nil, groupingKey: nil, groupInfo: nil, threadId: nil, timestamp: 0, flags: [], tags: [], globalTags: [], localTags: [], forwardInfo: nil, author: nil, text: "", attributes: [], media: [], peers: SimpleDictionary(), associatedMessages: SimpleDictionary(), associatedMessageIds: [], associatedMedia: [:])
-        
-        
-        let source = GalleryControllerItemSource.standaloneMessage(message)
         let size = CGSize(width:1280,height:720)
-
-        
         let updatedContent: TelegramMediaWebpageContent = .Loaded(TelegramMediaWebpageLoadedContent(url: "https://www.youtube.com/watch?v=idvMUlErPlA&ab_channel=PeppaPig-OfficialChannel", displayUrl: "youtube.com/watch?v=idvMUlErPlA", hash: 0, type: "video", websiteName: "YouTube", title: "🔴 LIVE Peppa Pig\'s Clubhouse 🐷🏠 BRAND NEW SEASON 9 PEPPA", text: "❤️ Watch the latest uploads here! https://www.youtube.com/playlist?list=PLFEgnf4tmQe_L3xlmtFwX8Qm5czqwCcVi\n❤️  Watch Peppa Pig\'s most Popular videos here! \nAbout Peppa Pig official channel:\n\nWelcome to the Official Peppa Pig channel and the home of Peppa on YouTube!  We have created a world of Peppa with episodes and compilations to keep even the most dedicated Peppa fans happy.  Here at Peppa Pig Official channel, kids can be happy and smart! Watch Peppa Pig clips and episodes where Peppa Pig play games with her friends, Mummy Pig and Daddy Pig tell the bedtime stories, Peppa Pig sings nursery rhymes and songs, Peppa and her family and friends celebrate Christmas and Halloween together! Peppa Pig episodic animation, Peppa   Pig songs for kids, Peppa Pig toy play and Peppa", embedUrl: "https://www.youtube.com/embed/idvMUlErPlA", embedType: "iframe", embedSize: PixelDimensions(size), duration: nil, author: nil, image: nil, file: nil, attributes: [], instantPage: nil))
-        let _ = TelegramMediaWebpage(webpageId: MediaId(namespace: 0, id: 1), content: updatedContent)
+        let webPage = TelegramMediaWebpage(webpageId: MediaId(namespace: 0, id: 1), content: updatedContent)
+
+//        let messageAttribute = MessageAttribute
+        //JP HACK
+        // attributes = ishdidden / type = Url / reactions
+        let message = Message(stableId: 0, stableVersion: 1, id: MessageId(peerId: PeerId(0), namespace: Namespaces.Message.Local, id: 0), globallyUniqueId: nil, groupingKey: nil, groupInfo: nil, threadId: nil, timestamp: 0, flags: [], tags: [], globalTags: [], localTags: [], forwardInfo: nil, author: nil, text: "", attributes: [], media: [webPage], peers: SimpleDictionary(), associatedMessages: SimpleDictionary(), associatedMessageIds: [], associatedMedia: [:])
         
         
-        let galleryVC = GalleryController(context: self.controller.accountContext(), source: source , invertItemOrder: false, streamSingleVideo: true, fromPlayingVideo: true, landscape: false, timecode: 0, playbackRate: 1, synchronousLoad: true, replaceRootController: { [weak navigationController] controller, ready in
-            navigationController?.replaceTopController(controller, animated: false, ready: ready)
+        // Source is message?
+                let source = GalleryControllerItemSource.standaloneMessage(message)
+                let galleryVC = GalleryController(context: self.controller.accountContext(), source: source , invertItemOrder: false, streamSingleVideo: true, fromPlayingVideo: true, landscape: false, timecode: 0, playbackRate: 1, synchronousLoad: true, replaceRootController: { _, ready in
+                    print("👹  we're in replaceRootController....")
+//            navigationController?.popToRoot(animated: true)//(controller, : false, ready: ready)
+                    self.controller?.navigationController?.popToRootViewController(animated: true)///(controller, : false, ready: ready)
+//                    navigationController?.popViewController(animated: true)
         }, baseNavigationController: navigationController, actionInteraction: nil)
-        galleryVC.temporaryDoNotWaitForReady = true
-        
-        let nc = UINavigationController(rootViewController: galleryVC)
-        self.controller.present(nc, animated: true)
+//        galleryVC.temporaryDoNotWaitForReady = true
+
+        self.controller.push(galleryVC)//(nc, animated: true)
 
         
         
